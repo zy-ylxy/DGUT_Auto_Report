8cb207c1614ea81ffcbd9d07098311cea2e23a65
BertraMoon <bertramoon@126.com>
2021-03-04 00:13:31 +0800

    test12

--- Auto_Report.py
+++ Auto_Report.py
@@ -6,55 +6,75 @@ import datetime
 import sys
 import time
 
+
+def Merge(dict1, dict2):
+    '''
+    合并两个字典
+    '''
+    res = {**dict1, **dict2}
+    return res
+
+
 try:
     username = sys.argv[1]
     password = sys.argv[2]
-    # data = json.loads(sys.argv[3])
-    print(username)
-    print(sys.argv[3])
-    print(type(sys.argv[3]))
+
+    card_number, tel, connect_person, connect_tel, family_address_detail = sys.argv[3].split(
+        ",")
+    form1 = {
+        'card_number': card_number,
+        'tel': tel,
+        'connect_person': connect_person,
+        'connect_tel': connect_tel,
+        'family_address_detail': family_address_detail,
+    }
+
+    fp = open("./data.json", 'rb')
+    form2 = json.loads(fp.read())
+    data = Merge(form1, form2)
+
     # while True:
     #     if (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).hour == 0 and (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).minute >= 20:
     #         break
     #     time.sleep(60)
 
     # 获取当日时间，修改字典
-    # now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
-    # data['submit_time'] = now.strftime("%Y-%m-%d")
+    now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+    data['submit_time'] = now.strftime("%Y-%m-%d")
 
-    # count = 1
-    # while True:
-    #     u = DgutLogin(username, password)
-    #     u.timeout = 30
+    count = 1
+    while True:
+        u = DgutLogin(username, password)
+        u.timeout = 30
 
-    #     # 登录
-    #     response = u.signin('illness')
-    #     print(response)
+        # 登录
+        response = u.signin('illness')
+        print(response)
 
-    #     # 获取access_token
-    #     access_token = re.search(
-    #         r'access_token=(.*)', u.login['illness']['response'][0][2].url, re.S).group(1)
-    #     print(f"access_token={access_token}")
+        # 获取access_token
+        access_token = re.search(
+            r'access_token=(.*)', u.login['illness']['response'][0][2].url, re.S).group(1)
+        print(f"access_token={access_token}")
 
-    #     # 构造headers
-    #     headers = {
-    #         'authorization': 'Bearer ' + access_token,
-    #         'Host': 'yqfk.dgut.edu.cn',
-    #         'Referer': 'https://yqfk.dgut.edu.cn/main',
-    #     }
+        # 构造headers
+        headers = {
+            'authorization': 'Bearer ' + access_token,
+            'Host': 'yqfk.dgut.edu.cn',
+            'Referer': 'https://yqfk.dgut.edu.cn/main',
+        }
 
-    #     # 提交
-    #     response = u.session.post(
-    #         "https://yqfk.dgut.edu.cn/home/base_info/addBaseInfo", headers=headers, json=data)
-    #     print("-"*20 + "结果" + "-"*20)
-    #     print(response)
-    #     print(response.text)
-    #     if response.status_code == 200:
-    #         break
-    #     if count > 10:
-    #         print("打卡失败")
-    #         raise Exception
-    #     time.sleep(60*5)
+        # 提交
+        response = u.session.post(
+            "https://yqfk.dgut.edu.cn/home/base_info/addBaseInfo", headers=headers, json=data)
+        print("-"*20 + "结果" + "-"*20)
+        print(response)
+        print(response.text)
+        if json.loads(response.text)['code'] in [200, 400]:
+            break
+        if count > 10:
+            print("打卡失败")
+            raise Exception
+        time.sleep(60*5)
 except IndexError:
     print("请完整输入账号、密码和提交的表单")
 except json.decoder.JSONDecodeError:
