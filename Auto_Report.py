6b63536d8f68bda55b98c544823cb985d9206547
BertraMoon <bertramoon@126.com>
2021-05-14 18:37:03 +0800

    update

--- Auto_Report.py
+++ Auto_Report.py
@@ -11,7 +11,9 @@ class dgutIll(dgutUser):
         }
 
     @decorator_signin(illness_login)
-    def report(self):
+    def report(self, longitude: float = 113.87651, latitude: float = 22.90701):
+        if not (isinstance(longitude, float) and isinstance(latitude, float)):
+            raise TypeError("经度longitude和纬度latitude只能是浮点数")
         # 1、获取access_token
         response = self.signin(illness_login)
         access_token = re.search(
@@ -20,12 +22,6 @@ class dgutIll(dgutUser):
             raise AuthError("获取access_token失败")
         access_token = access_token.group(1)
         self.session.headers['authorization'] = 'Bearer ' + access_token
-
-        # GPS
-        response = self.session.get(
-            "https://yqfk.dgut.edu.cn/home/base_info/getGPSAddress?longitude=113.87651&latitude=22.90701&reject=1")
-        print("GPS:")
-        print(response, response.text)
 
         # 2、获取并修改数据
         response = self.session.get(
@@ -53,9 +49,13 @@ class dgutIll(dgutUser):
             'whitelist',
             'importantAreaMsg',
         ]
-        for key in pop_list:
-            if data.get(key):
-                data.pop(key)
+        [data.pop(key) for key in pop_list if data.get(key)]
+
+        # GPS
+        response = self.session.get(
+            "https://yqfk.dgut.edu.cn/home/base_info/getGPSAddress?longitude=113.87651&latitude=22.90701")
+        print("GPS:")
+        print(response, response.text)
 
         # 3、提交数据
         response = self.session.post(
@@ -66,14 +66,15 @@ class dgutIll(dgutUser):
 @click.command()
 @click.option('-U', '--username', required=True, help="中央认证账号用户名", type=str)
 @click.option('-P', '--password', required=True, help="中央认证账号密码", type=str)
-def main(username, password):
+@click.option('-L', '--location', help="经纬度", nargs=2, type=float, default=(113.87651, 22.90701))
+def main(username, password, location):
     users = username.split(",")
     pwds = password.split(",")
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
     for usr in zip(users, pwds):
         u = dgutIll(usr[0], usr[1])
-        print(u.report())
+        print(u.report(location[0], location[1]))
 
 
 if __name__ == '__main__':
