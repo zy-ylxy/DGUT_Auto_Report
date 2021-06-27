06e9807e4b0ef2837d951a552cf4bcbfe95f7f63
BertraMoon <bertramoon@126.com>
2021-06-27 22:01:30 +0800

    fix the bug

--- Auto_Report.py
+++ Auto_Report.py
@@ -5,11 +5,12 @@ from dgut_requests.dgut import dgutIllness
 @click.command()
 @click.option('-U', '--username', required=True, help="中央认证账号用户名", type=str)
 @click.option('-P', '--password', required=True, help="中央认证账号密码", type=str)
-@click.option('-L', '--location', help="经纬度", type=(int, float, float), multiple=True)
+@click.option('-L', '--location', help="经纬度")
 def main(username, password, location):
     users = username.split(",")
     pwds = password.split(",")
-    locations = {item[0]: (item[1], item[2]) for item in location}
+    locations = {int(item.split(',')[0]): (float(item.split(',')[1]), float(item.split(
+        ',')[2])) for item in location.strip('[]').split('],[')} if location else {}
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
     for usr in enumerate(zip(users, pwds), 1):
