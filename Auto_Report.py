17891e3f2a8092ac638ad92cba70996fdcbdd4e8
BertraMoon <bertramoon@126.com>
2021-06-27 21:39:43 +0800

    更新了多账号经纬度设置

--- Auto_Report.py
+++ Auto_Report.py
@@ -5,15 +5,19 @@ from dgut_requests.dgut import dgutIllness
 @click.command()
 @click.option('-U', '--username', required=True, help="中央认证账号用户名", type=str)
 @click.option('-P', '--password', required=True, help="中央认证账号密码", type=str)
-@click.option('-L', '--location', help="经纬度", nargs=2, type=float, default=(113.87651, 22.90701))
+@click.option('-L', '--location', help="经纬度", type=(int, float, float), multiple=True)
 def main(username, password, location):
     users = username.split(",")
     pwds = password.split(",")
+    locations = {item[0]: (item[1], item[2]) for item in location}
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
-    for usr in zip(users, pwds):
-        u = dgutIllness(usr[0], usr[1])
-        print(u.report(location[0], location[1]))
+    for usr in enumerate(zip(users, pwds), 1):
+        u = dgutIllness(usr[1][0], usr[1][1])
+        if locations.get(usr[0]):
+            print(u.report(locations.get(usr[0])[0], locations.get(usr[0])[1]))
+        else:
+            print(u.report())
 
 
 if __name__ == '__main__':
