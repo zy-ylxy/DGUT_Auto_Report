aaa1421775b32f667ea250bd6d75d2cfdd856539
BertraMoon <bertramoon@126.com>
2021-07-13 01:16:08 +0800

    修改了LOCATIONS的用法

--- Auto_Report.py
+++ Auto_Report.py
@@ -10,7 +10,7 @@ def main(username, password, location):
     users = username.split(",")
     pwds = password.split(",")
     locations = {int(item.split(',')[0]): (float(item.split(',')[1]), float(item.split(
-        ',')[2])) for item in location.strip('[]').split('],[')} if location else {}
+        ',')[2])) for item in location.strip('[] ').split('],[')} if location else {}
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
     for usr in enumerate(zip(users, pwds), 1):
