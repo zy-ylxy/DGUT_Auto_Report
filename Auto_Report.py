877612bb42da089cbde6da63a2ec5ab761808586
bertramoon <bertramoon@126.com>
2021-10-08 17:25:21 +0800

    添加了gevent异步操作，减少多账号下的响应时间

--- Auto_Report.py
+++ Auto_Report.py
@@ -1,23 +1,23 @@
+from gevent import monkey;monkey.patch_all()
 import logging
 import click
 from dgut_requests.dgut import dgutIllness
 from retry import retry
+import gevent
 
 
 @retry(tries=50, delay=2, backoff=2, max_delay=30)
 def clock(u: dgutIllness, location: "list | None" = None) -> None:
     '''
     打卡并输出结果
-
-
     :param u: dgutIllness, 指定打卡的dgutIllness对象.
     :param location: list | None, 指定打卡的定位（经纬度列表类型）或缺省None.
     :returns: None，打印结果
     '''
     if location:
-        print(u.report(location[0], location[1]))
+        print(u.username[-2:], '-', u.report(location[0], location[1]))
     else:
-        print(u.report())
+        print(u.username[-2:], '-', u.report())
 
 
 @click.command()
@@ -31,13 +31,11 @@ def main(username, password, location):
         ',')[2])) for item in location.strip('[] ').split('],[')} if location else {}
     if len(users) != len(pwds):
         exit("账号和密码个数不一致")
+    tasks = []
     for usr in enumerate(zip(users, pwds), 1):
         u = dgutIllness(usr[1][0], usr[1][1])
-        if locations.get(usr[0]):
-            clock(u, locations.get(usr[0]))
-        else:
-            clock(u)
-
+        tasks.append(gevent.spawn(clock, u=u, location=locations.get(usr[0])))
+    gevent.joinall(tasks)
 
 if __name__ == '__main__':
     main()
